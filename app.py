4513e8d1ce9b7b4bb9dfd9812777bec8692ba7eb
Satish <satish.brainiac@gmail.com>
2025-08-17 11:55:23 +0530

    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+from flask import Flask, render_template, request, redirect, session, jsonify
 import database
 
 app = Flask(__name__)
